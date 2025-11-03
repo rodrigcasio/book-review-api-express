825b839e1e103c97337feb1700cb8eb57562587d
rodrigcasio <rod.casio@outlook.com>
2025-11-02 20:19:53 -0600

    Added favorite books for my book object database within the directory `/data/booksdb.js`

---
+++ book-review-api/data/booksdb.js
@@ -0,0 +1,29 @@
+// some books ive read and want to read
+let books = {
+    1: {"author": "Ransom Riggs","title": "Miss Peregrine's Home for Peculiar Children", "reviews": {} },
+    2: {"author": "Wendelin Van Draanen","title": "The Running Dream", "reviews": {} },
+    3: {"author": "Matt Haig","title": "How to Stop Time", "reviews": {} },
+    4: {"author": "Camilla Läckberg","title": "The Girl in the Woods (Patrik Hedström, #10)", "reviews": {} },
+    5: {"author": "Stephen King","title": "The Outsider", "reviews": {} },
+    6: {"author": "Levy Rozman","title": "How to Win at Chess: The Ultimate Guide for Beginners and Beyond", "reviews": {} },
+    7: {"author": "Nathan Clark","title": "Computer Programming for Beginners: Fundamentals of Programming", "reviews": {} },
+    8: {"author": "John Stoddard","title": "Quantum Physics for Beginners, Into the Light: The 4 Bizarre Discoveries..", "reviews": {} },
+    9: {"author": "John Stoddard","title": "Relativity for Beginners, The Special And The General Theory", "reviews": {} },
+    10: {"author": "Richard P. Feynman","title": "Six Easy Pieces: Essentials of Physics Explained...", "reviews": {} },
+    11: {"author": "Robert Nystrom","title": "Crafting Interpreters", "reviews": {} },
+    12: {"author": "Christopher Negus","title": "Linux Bible", "reviews": {} },
+    13: {"author": "Robert C. Seacord","title": "Effective C: An Introduction to Professional C Programming", "reviews": {} },
+    14: {"author": "Charles Petzold","title": "Code: The Hidden Language of Computer Hardware and Software", "reviews": {} },
+    15: {"author": "Brian W. Kernighan","title": "The UNIX Programming Environment", "reviews": {} },
+    16: {"author": "Robert C. Martin","title": "Clean Code: A Handbook of Agile Software Craftsmanship", "reviews": {} },
+    17: {"author": "Oliver Byrne","title": "The First Six Books of the Elements of Euclid", "reviews": {} },
+    18: {"author": "Steven Weinberg","title": "The Discovery of Subatomic Particles", "reviews": {} },
+    19: {"author": "David J. Griffiths","title": "Introduction to Electrodynamics", "reviews": {} },
+    20: {"author": "David J. Griffiths","title": "Introduction to Elementary Particles", "reviews": {} },
+    21: {"author": "Richard Blum","title": "Professional Assembly Language", "reviews": {} },
+    22: {"author": "Robert Love","title": "Linux Kernel Development", "reviews": {} },
+    23: {"author": "Ellen Siever","title": "Linux", "reviews": {} },
+    24: {"author": "Daniel P. Bovet","title": "Understanding the Linux Kernel", "reviews": {} }
+}
+
+module.exports = books;
